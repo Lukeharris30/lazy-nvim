c4a06d659959f9255fb00c706eb3336a132b4578
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2025-08-15 13:59:37 -0600

    feat: telescope and space as a leader

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
+vim.keymap.set("", "<Space>", "<Nop>", {desc = "Nop on space because it's a leader"})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.lazy")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
